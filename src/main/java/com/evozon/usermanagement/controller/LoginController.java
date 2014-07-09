52779d6391644c57f3f561ca3bc78b1b044483eb
biancadragoescu <bianca.dragoescu@evozon.com>
2014-07-09 12:00:42 +0100

    First commit

---
+++ src/main/java/com/evozon/usermanagement/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.evozon.usermanagement.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+//private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Locale locale, Model model) {
+		return "login";
+	}
+}
